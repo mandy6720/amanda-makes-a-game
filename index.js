d627f28dd56f3c34666cee60ab7530001bd1f2a9
Amanda Bozzi <amanda.hewitt01@gmail.com>
2021-10-29 12:07:59 -0500

    Add styles and inital JS

---
+++ index.js
@@ -0,0 +1,12 @@
+let level;
+
+const initializeGame = () => {
+  level = 1;
+
+  console.log(level);
+};
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  //do work son
+  initializeGame();
+});
